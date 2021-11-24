8e31586accffcf5627324e9a090f5e2945c1625e
k-e-r <damajonboua@gmail.com>
2021-11-23 22:26:20 -0800

    add cast of code

--- demo/src/main/java/com/example/demo/SampleService.java
+++ demo/src/main/java/com/example/demo/SampleService.java
@@ -59,7 +59,8 @@ public class SampleService {
     try {
       connection = DriverManager.getConnection(dbUrl, username, password);
       delete(productForm);
-      PreparedStatement statement = connection.prepareStatement("INSERT INTO product VALUES (?, ?, ?, ?, ?)");
+      PreparedStatement statement = connection
+          .prepareStatement("INSERT INTO product VALUES ((cast(? as int), ?, ?, ?, ?)");
 
       statement.setString(1, productForm.getCode());
       statement.setString(2, productForm.getName());
@@ -88,8 +89,8 @@ public class SampleService {
 
     try {
       connection = DriverManager.getConnection(dbUrl, username, password);
-      PreparedStatement statement = connection
-          .prepareStatement("UPDATE product SET code=?, name=?, description=?, price=?, evaluation=? WHERE code=?");
+      PreparedStatement statement = connection.prepareStatement(
+          "UPDATE product SET code=(cast(? as int), name=?, description=?, price=?, evaluation=? WHERE code=?");
 
       statement.setString(1, productForm.getCode());
       statement.setString(2, productForm.getName());
@@ -119,7 +120,7 @@ public class SampleService {
 
     try {
       connection = DriverManager.getConnection(dbUrl, username, password);
-      PreparedStatement statement = connection.prepareStatement("DELETE FROM product WHERE code=?");
+      PreparedStatement statement = connection.prepareStatement("DELETE FROM product WHERE code=(cast(? as int)");
 
       statement.setString(1, productForm.getCode());
 
